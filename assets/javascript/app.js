e5ff11eff913ba4db3c182d2d07c0d989dff49a6
syeda zaidi <zaidisyeda03@gmail.com>
2019-07-10 18:33:39 -0700

    start page in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,50 @@
+$(document).ready(function() {
+    
+    var allQuestions = [{
+        question: "How many Districts are there in Panem ?",
+        options: ["5", "12", "15", "8"],
+        correctAnswer: "12",
+    },
+    {
+        question: "Who is the president of Panem ?",
+        options: ["Haymitch", "Plutarch Heavensbee", "Coriolanus Snow", "Alma Coin"],
+        correctAnswer: "Coriolanus Snow",
+    },
+    {
+        question: "What is the name of Katniss Everdeen's sister ?",
+        options: ["Primrose", "Rue", "Johanna", "Annie"],
+        correctAnswer: "Primrose",
+    },
+    {
+        question: "What industry is district 12 responsible for ?",
+        options: ["Farming", "Weapons", "Fishing", "Coal mining"],
+        correctAnswer: "Coal mining",
+    },
+];
+console.log (allQuestions);
 
-    alert ("hi");
-    console.log ("hello");
+var currentQuestion;
+var correctAnswer;
+var incorrectAnswer;
+var answerOptions;
+
+
+$("#startbtn").on("click", function() {
+    $("#startbtn").hide();
+    startgame();
+});
+
+
+
+
+
+function startgame() {
+    currentQuestion = allQuestions[0].question;
+    $("#display-question").text(currentQuestion);
+
+    answerOptions = allQuestions[0].options;
+    $("#display-options").html(answerOptions );
+};
+
+
+});
