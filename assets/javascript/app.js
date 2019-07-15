8a87ea703cdc7b665151096d827ec5ce736b576b
syeda zaidi <zaidisyeda03@gmail.com>
2019-07-14 20:59:16 -0700

    added image, css and added js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
     var timer;
     var Question;
 
-
+    //runs the timer, and loads next question if timer reaches 0 
     function timerCount() {
         countDown--;
         $("#display-time").text("Time Remaining : " + countDown);
@@ -41,18 +41,20 @@ $(document).ready(function () {
         if (countDown === 0) {
             clearInterval(timer);
             unanswered++;
-            console.log ("unanswered questions " + unanswered);
+            console.log("unanswered questions " + unanswered);
             nextQuestion();
         }
     }
 
+    //on click the game starts 
     $("#startbtn").on("click", function () {
         $("#startbtn").hide();
         startgame();
     });
 
+    // loads the questions and choices 
     function startgame() {
-        clearInterval(timer); 
+        clearInterval(timer);
         countDown = 5;
         timer = setInterval(timerCount, 1000);
 
@@ -67,7 +69,7 @@ $(document).ready(function () {
 
     function displayOptions() {
         answerOptions = allQuestions[currentQuestion].options;
-        
+
         for (i = 0; i < answerOptions.length; i++) {
             var choices = $("<button>");
             choices.attr("data-answer", answerOptions[i]);
@@ -77,21 +79,23 @@ $(document).ready(function () {
         }
     };
 
+    //event listener for chosen answer, and loads the next question 
     $(document).on("click", ".choices", function () {
-        if (($(this).attr("data-answer")) === allQuestions[currentQuestion].correctAnswer) {
+        var selectedAnswer = $(this).attr("data-answer");
+        if (selectedAnswer === allQuestions[currentQuestion].correctAnswer) {
             alert("correct answer");
             correctAnswer++;
             console.log("correct answers" + correctAnswer);
             nextQuestion();
         } else {
-            alert ("wrong answer");
+            alert("wrong answer");
             incorrectAnswer++;
             console.log("wrong answers " + incorrectAnswer);
             nextQuestion();
         }
-        });
-    
+    });
 
+    //loads the next question, if no more questions then ends the game 
     function nextQuestion() {
 
         if (currentQuestion === allQuestions.length - 1) {
@@ -105,14 +109,38 @@ $(document).ready(function () {
         }
     };
 
-
+    // at the end of all questions, empties all html and stops the timer
     function endGame() {
         clearInterval(timer);
         $("#display-time").empty();
         $("#display-question").empty();
         $("#display-options").empty();
 
+        displayResult();
+
+    };
+    //displays the final results 
+    function displayResult() {
+        var results = `
+        <p> Correct Answers : ${correctAnswer} </p>
+        <p> Incorrect Answers : ${incorrectAnswer} </p>
+        <p> Unanswered Questions : ${unanswered} </p>
+        <button id="resetbtn" class="btn btn-primary"> Reset Game </button>
+        `;
+        $("#display-result").html(results);
+        console.log("game over");
+        $("#display-result").show();
+
     };
 
+    $(document).on("click", "#resetbtn", function () {
+        $("#display-result").hide();
+        currentQuestion = 0;
+        correctAnswer = 0;
+        incorrectAnswer = 0;
+        unanswered = 0;
+        startgame();
+
+    });
 
 });
